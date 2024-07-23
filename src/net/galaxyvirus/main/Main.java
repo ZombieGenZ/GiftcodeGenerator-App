3d919cb2c7cc61faf26d457b504e571405083270
ZombieGenZ <zombiegenzzz@gmail.com>
2024-07-23 20:54:24 +0700

    Update

--- src/net/galaxyvirus/main/Main.java
+++ src/net/galaxyvirus/main/Main.java
@@ -8,22 +8,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Nhập tên dự án: ");
+        System.out.print("Enter project name: ");
         String fileName = sc.nextLine();
-        System.out.print("Nhập giá trị nhỏ nhất: ");
+        System.out.print("Enter the minimum value: ");
         int min = sc.nextInt();
-        System.out.print("Nhập giá trị lớn nhất: ");
+        System.out.print("Enter the maximum value: ");
         int max = sc.nextInt();
-        System.out.print("Nhập số lượng mã cần tạo: ");
+        System.out.print("Enter the quantity to generator: ");
         int quantity = sc.nextInt();
         try {
             File myFile = new File("result/" + fileName.trim() + ".result.txt");
             if (myFile.createNewFile()) {
-                System.out.println("Đã tạo file dự án mới với tên " + myFile.getName());
+                System.out.println("Created project file with name " + myFile.getName());
             }
             try {
-                FileWriter myFileWriter = new FileWriter("result/" + fileName.trim() + ".result.txt");
-                FileWriter overviewFileWriter = new FileWriter("result/" + fileName.trim() + ".list.txt");
+                FileWriter myFileWriter = new FileWriter("./result/" + fileName.trim() + ".result.txt");
+                FileWriter overviewFileWriter = new FileWriter("./result/" + fileName.trim() + ".list.txt");
 
                 int giftcodeLength = 12;
 
@@ -59,7 +59,7 @@ public class Main {
                         myFileWriter.write("\t\"" + giftcode + "\": {\n\t\t\"giftcode\": \"" + giftcode + "\",\n\t\t\"min\": " + min + ",\n\t\t\"max\": " + max + "\n\t},\n");
                         overviewFileWriter.write("\t" + giftcode + "\n");
                     }
-                    System.out.println("Đã xuất giftcode số " + i + " hoàn tất");
+                    System.out.println("Completed export of giftcode number " + i);
                 }
                 myFileWriter.close();
                 overviewFileWriter.close();
@@ -67,16 +67,16 @@ public class Main {
                 long endTime = System.currentTimeMillis();
                 long elapsedTime = endTime - startTime;
 
-                System.out.println("Đã xuất hoàn tất " + quantity + " mã json giftcode vào đường dẩn: result/" + fileName.trim() + "/json.txt");
-                System.out.println("Đã xuất hoàn tất " + quantity + " mã giftcode vào đường dẩn: result/" + fileName.trim() + "/json.txt");
+                System.out.println("Completed export of " + quantity + " json giftcode codes onto the path: result/" + fileName.trim() + "/json.txt");
+                System.out.println("Completed export of " + quantity + " giftcode lists onto the path: result/" + fileName.trim() + "/json.txt");
 
-                System.out.println("Thời gian thực hiện: " + elapsedTime + " ms");
+                System.out.println("Completed in: " + elapsedTime + " ms");
             } catch (IOException e) {
-                System.out.println("Lỗi khi xử lý file.");
+                System.out.println("Error while processing file.");
                 e.printStackTrace();
             }
         } catch (IOException e) {
-            System.out.println("Lỗi khi đọc file file.");
+            System.out.println("Error when reading file.");
             e.printStackTrace();
         }
     }
